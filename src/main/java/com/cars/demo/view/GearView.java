f93aa94a0d3f9fd26212276b46c74d2a9934d50f
ismael1987 <asmael-1987@hotmail.com>
2018-07-16 17:28:53 +0200

    add GearView and gear.htmi

---
+++ src/main/java/com/cars/demo/view/GearView.java
@@ -0,0 +1,34 @@
+package com.cars.demo.view;
+
+import com.cars.demo.model.Gear;
+import com.cars.demo.repository.GearRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class GearView {
+
+    @Autowired
+    GearRepository gearRepository;
+
+    @GetMapping("/gear")
+    public String getgear(Model model){
+        List<Gear> CollectGear = gearRepository.findAll().stream().collect(Collectors.toList());
+        model.addAttribute("gears",CollectGear);
+        return "gear";
+    }
+
+    @PostMapping("gear")
+    public String addgear(@Valid String gear){
+        Gear newgear = new Gear(gear);
+        gearRepository.save(newgear);
+        return "index";
+    }
+}
