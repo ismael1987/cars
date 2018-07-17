442c48bf0863de3a1084e55209623ebc659b507e
ismael1987 <asmael-1987@hotmail.com>
2018-07-17 17:39:15 +0200

    add brand.html

---
+++ src/main/java/com/cars/demo/model/Brand.java
@@ -0,0 +1,45 @@
+package com.cars.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Brand {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String brandName;
+
+    public Brand() {
+    }
+
+    public Brand(Long id, String brandName) {
+        this.id = id;
+        this.brandName = brandName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{" +
+                "id=" + id +
+                ", brandName='" + brandName + '\'' +
+                '}';
+    }
+}
