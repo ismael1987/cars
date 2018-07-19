d38fd799ccc200f6f6c31083a56a82c7eb8e7942
ismael1987 <asmael-1987@hotmail.com>
2018-07-19 18:19:18 +0200

    add function to general class

--- src/main/java/com/cars/demo/view/general.java
+++ src/main/java/com/cars/demo/view/general.java
@@ -1,6 +1,8 @@
 package com.cars.demo.view;
 
+import com.cars.demo.model.Modell;
 import com.cars.demo.model.State;
+import com.cars.demo.repository.ModelRepository;
 import com.cars.demo.repository.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +19,18 @@ public class general {
 
     @Autowired
     StateRepository stateRepository;
+    @Autowired
+    ModelRepository modelRepository;
 
     @GetMapping("getState")
     List<State> getState(@Valid Long country){
 
         return stateRepository.findAll().stream().filter(state -> state.getCountry().getId().equals(country)).collect(Collectors.toList()) ;
     }
+
+    @GetMapping("getmodel")
+    List<Modell> getmodel(@Valid Long brand){
+        return  modelRepository.findAll().stream().filter(modell -> modell.getBrand().getId().equals(brand)).collect(Collectors.toList());
+    }
+
 }
