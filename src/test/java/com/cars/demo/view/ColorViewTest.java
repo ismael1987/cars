0e999f8d33130e0dbf2d0fe21583a301d95e93e7
ismael1987 <asmael-1987@hotmail.com>
2018-07-26 17:42:31 +0200

    work on color testing

--- src/test/java/com/cars/demo/view/ColorViewTest.java
+++ src/test/java/com/cars/demo/view/ColorViewTest.java
@@ -1,52 +1,95 @@
 package com.cars.demo.view;
 
 import com.cars.demo.model.Color;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import com.cars.demo.service.RepositoryServices;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebMvcTest(controllers = ColorView.class)
+@WebAppConfiguration
 class ColorViewTest {
 
-    @LocalServerPort
-    private int port;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
-    private TestRestTemplate restTemplate;
-
-
-    @SpyBean
     private ColorView colorView;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
-    private String endpoint = "/colors";
+    @MockBean
+    private ColorView getColorView;
 
-    private String url;
+    @Mock
+    private RepositoryServices repositoryServices;
 
-    @BeforeEach
-    void before() {
-        url = "http://localhost:" + port + endpoint;
-     }
+    @Mock
+    private Model model;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
-    void getcolor() {
-        ResponseEntity<Object[]> forEntity = restTemplate.getForEntity(url, Object[].class);
+    public void getcolor() throws Exception{
 
-        //List<Color> psets = Arrays.asList(response.getBody());
-        assertEquals(2,2);
-        //verify(colorView).getcolor();
+        Color color1 = new Color();
+        color1.setId(1L);
+        color1.setDescription("red");
+
+        Color color2 = new Color();
+        color2.setId(2L);
+        color2.setDescription("blue");
+
+        when(colorView.getcolors()).thenReturn(Arrays.asList(color1,color2));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/colors"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("color"))
+                .andExpect(model().attribute("colors", hasSize(2)))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("colors"))
+                .andExpect(model().attribute("colors",hasItem(
+                        allOf(
+                                hasProperty("id",is(1L)),
+                                hasProperty("description" ,  is("red"))
+                        )
+                )))
+                .andExpect(model().attribute("colors",hasItem(
+                        allOf(
+                                hasProperty("id",is(2L)),
+                                hasProperty("description" ,  is("blue"))
+                        )
+                )))
+                .andDo(print());
+
     }
+
+
 
 }
