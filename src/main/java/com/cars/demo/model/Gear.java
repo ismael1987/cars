4bc72840055e3509e8d87b1e9bf65bbbe73d1bb2
ismael1987 <asmael-1987@hotmail.com>
2018-07-31 23:47:47 +0200

    add annotation to Gear class

--- src/main/java/com/cars/demo/model/Gear.java
+++ src/main/java/com/cars/demo/model/Gear.java
@@ -1,8 +1,11 @@
 package com.cars.demo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Gear {
@@ -11,6 +14,9 @@ public class Gear {
     @GeneratedValue
     private Long id;
 
+    @NotNull
+    @Column(nullable = false ,  unique = true)
+    @Size(min=1 , max=255)
     private String geartype;
 
     public Gear() {
