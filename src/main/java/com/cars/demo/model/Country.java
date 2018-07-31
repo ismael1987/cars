fe149ec4ac5b7c6ccd311181b728820d91fd00e0
ismael1987 <asmael-1987@hotmail.com>
2018-07-31 23:44:05 +0200

    add annotation to country class

--- src/main/java/com/cars/demo/model/Country.java
+++ src/main/java/com/cars/demo/model/Country.java
@@ -1,14 +1,21 @@
 package com.cars.demo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Country {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotNull
+    @Column(nullable = false ,  unique = true)
+    @Size(min=1 , max=255)
     private String countryName;
 
     public Country() {
