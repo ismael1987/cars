471864ee7e77ac721e15234a7ef386fbe62b20a0
ismael1987 <asmael-1987@hotmail.com>
2018-07-31 23:49:11 +0200

    add annotation to typeOfCar class

--- src/main/java/com/cars/demo/model/TypeOfCar.java
+++ src/main/java/com/cars/demo/model/TypeOfCar.java
@@ -1,8 +1,11 @@
 package com.cars.demo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class TypeOfCar {
@@ -10,6 +13,10 @@ public class TypeOfCar {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotNull
+    @Column(nullable = false ,  unique = true)
+    @Size(min=1 , max=255)
     private String carType;
 
     public TypeOfCar() {
