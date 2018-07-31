1ea25c27b3bcb33ce2de878af4a0021a0bf13278
ismael1987 <asmael-1987@hotmail.com>
2018-07-31 23:41:58 +0200

    add annotations to Brand class

--- src/main/java/com/cars/demo/model/Brand.java
+++ src/main/java/com/cars/demo/model/Brand.java
@@ -1,14 +1,21 @@
 package com.cars.demo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Brand {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotNull
+    @Column(nullable = false ,  unique = true)
+    @Size(min=1 , max=255)
     private String brandName;
 
     public Brand() {
